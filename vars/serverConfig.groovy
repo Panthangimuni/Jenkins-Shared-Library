248acba8ed2a6129063647d0d0b6173f38d11ba7
Panthangimuni <101710573+Panthangimuni@users.noreply.github.com>
2025-07-22 15:49:24 +0530

    Update serverConfig.groovy

--- vars/serverConfig.groovy
+++ vars/serverConfig.groovy
@@ -1,19 +1,19 @@
 // vars/serverConfig.groovy
 def getServerPaths() {
     return [
-        "13.204.76.47": [
+        "52.66.138.178": [
             "frontend": "/var/www-app/frontend",
             "backend": "/var/www-app/backend",
             "api-server": "/var/www-app/api-server",
             "report": "/var/www-app/report"
         ],
-        "13.234.66.140": [
+        "65.0.61.156": [
             "muni-server": "/var/www-muni/muni-server",
             "narayna-server": "/var/www-narayna/narayna-server",
             "ram-server": "/var/www-ram/ram-server",
             "rathnam-server": "/var/www-app/rathnam-server"
         ],
-        "3.108.250.203": [
+        "65.0.87.184": [
             "naresh-server": "/var/www-php/naresh-server",
             "ravi-server": "/var/www-php/ravi-server",
             "saravana-server": "/var/www-app/saravana-server",
